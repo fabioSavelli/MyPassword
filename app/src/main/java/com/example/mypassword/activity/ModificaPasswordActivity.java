36259c194c60fd89793553cfb2576f69b47e31a5
Fabio Savelli <>
2021-08-27 22:11:02 +0200

    creazione activity vuote

---
+++ app/src/main/java/com/example/mypassword/activity/ModificaPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.mypassword.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mypassword.R;
+
+public class ModificaPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifica_password);
+    }
+}
